01fbad05d3f3740fe6fb227db7eaa3cf713c6712
Batman <sirati10546@gmail.com>
2022-06-11 15:23:35 +0700

    ปรับปรุงความปลอดภัยของระบบ

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,8 +2,9 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import * as dotenv from 'dotenv';
 import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDoc } from 'src/users/entities/user.entity';
+import { User, UserDoc } from '../../users/entities/user.entity';
 import { Model } from 'mongoose';
+import { UnauthorizedException } from '@nestjs/common';
 dotenv.config();
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(@InjectModel(User.name) private userModel: Model<UserDoc>) {
@@ -16,6 +17,9 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
   async validate(payload: { sub: string; username: string }) {
     const user: any = await this.userModel.findOne({ _id: payload.sub });
+    if (!user) {
+      throw new UnauthorizedException();
+    }
     return user;
   }
 }
